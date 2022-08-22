b10094ea2542a8e278fbae9dd83800571775393a
Justin Jeffries <justincyjeffries@gmail.com>
2022-08-22 16:50:43 -0400

    adds routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,48 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+  #portfolio routes
+  get '/portfolios' do
+    portfolios = Portfolio.all
+    portfolios.to_json
+  end
+  get '/portfolios:id' do
+    portfolio = Portfolio.find(params[:id])
+    portfolio.to_json
+  end
+  post '/portfolios' do
+    portfolio = Portfolio.create(
+      name: params[:name],
+      timestamps: params[:timestamps]
+    )
+    portfolio.to_json
+  end
+  #crypto routes
+  get '/cryptos' do
+    cryptos = Crypto.all
+    cryptos.to_json
+  end
+  get '/cryptos:id' do
+    crypto = Crypto.find(params[:id])
+    crypto.to_json
+  end
+  #purchase routes
+  get '/purchases' do
+    purchases = Purchase.all
+    purchases.to_json
+  end
+  get '/purchases:id' do
+    purchase = Purchase.find(params[:id])
+    purchase.to_json
+  end
+  delete '/purchases:id' do
+    purchase = Purchase.find(params[:id])
+    purchase.destroy
+    purchase.to_json
+  end
+
+
+
+
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
