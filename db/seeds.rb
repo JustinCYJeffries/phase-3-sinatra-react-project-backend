ad70f326d9795f82d491c21f7ce9d8854138b524
Justin Jeffries <justincyjeffries@gmail.com>
2022-08-23 22:28:13 -0400

    fixes syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ cryptolist = Cryptocompare::CoinList.all
 cryptoKeys = cryptolist["Data"].keys
 cryptoKeys.each do |crypt|
     Crypto.create(
-        name: cryptolist["Data"][`${crypt}`]["FullName"]
-        ticker: crypt
-        price: Cryptocompare::Price.full(`${crypt}`, 'USD')["RAW"][`${crypt}`]["USD"]["PRICE"]
-        volume: Cryptocompare::Price.full(`${crypt}`, 'USD')["RAW"][`${crypt}`]["USD"]["VOLUME24HOUR"]
+        name: cryptolist["Data"]["#{crypt}"]["FullName"]
+        ticker: "#{crypt}"
+        price: Cryptocompare::Price.full("#{crypt}", 'USD')["RAW"]["#{crypt}"]["USD"]["PRICE"]
+        volume: Cryptocompare::Price.full("#{crypt}", 'USD')["RAW"]["#{crypt}"]["USD"]["VOLUME24HOUR"]
     )
 
 puts "✅ Done seeding!"
